aff19ece49969113b84fbfeb0100f7385c149ce8
Adil <seyidli0604@gmail.com>
2026-08-24 21:45:21 +0400

    chore: initialize Spring Boot project

---
+++ src/test/java/com/adil/ordertracker/OrderTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adil.ordertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
